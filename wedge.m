9ee3ebff6390ab085ccbf87cfa083d20c9312100
OverflowCat <overflowcat@gmail.com>
2023-12-11 20:51:34 +0800

    凹陷

--- wedge.m
+++ wedge.m
@@ -5,7 +5,7 @@ times = 0;
 
 lambda = 6e-5; % 波长，为 600nm
 scratch_depth = 1/3 * lambda; % 划痕深度
-scratch_width = 0.0001;
+scratch_width = 0.0002;
 
 for theta = 0.1:0.005:0.4 % 劈尖夹角
     times = times + 1;
@@ -19,19 +19,31 @@ for theta = 0.1:0.005:0.4 % 劈尖夹角
     scratch_center = length / 2; % 划痕中心位置
     scratch_start = scratch_center - scratch_width / 2; % 划痕开始位置
     scratch_end = scratch_center + scratch_width / 2; % 划痕结束位置
-    scratch_area = x > scratch_start & x < scratch_end; % 定义划痕区域
 
     scratch_length = 0.0005; % 划痕的长度，例如 0.5 毫米
     scratch_y_center = 0.0005; % 划痕沿 y 轴的中心位置，例如在 0.5 毫米处
     scratch_y_start = scratch_y_center - scratch_length / 2;
     scratch_y_end = scratch_y_center + scratch_length / 2;
-    scratch_area_y = y > scratch_y_start & y < scratch_y_end;
-    scratch_area = scratch_area & scratch_area_y;
 
-    h = x * tan(theta); % 在 x 处的高度
+    % 定义划痕区域
+    scratch_area_x = x > scratch_start & x < scratch_end;
+    scratch_area_y = y > scratch_y_start & y < scratch_y_end;
+    scratch_area = scratch_area_x & scratch_area_y;
+
+    % 定义划痕形状 - 三次曲面
+    % 系数 'a' 决定曲面的弯曲度，确保在划痕中心深度为 scratch_depth
+    a = -4 * scratch_depth / (scratch_width / 2)^3;
+    % 三次曲面方程，中心化在划痕中心
+    z_scratch = a * (x - scratch_center).^3 + scratch_depth;
+
+    % 在划痕区域外，z_scratch 设为 0
+    z_scratch(~scratch_area) = 0;
+
+    % 更新劈尖处的高度 h 以包括划痕
+    h = x * tan(theta) + z_scratch; % 在 x 处的高度
+
     k = 2 * pi / lambda; % 波数
     Delta = 2 * h; % 光程差
-    Delta(scratch_area) = Delta(scratch_area) + scratch_depth; % 在划痕区域增加厚度
     I = I0 * 2 * (cos(Delta * k + pi) + 1);
     surf(x, y, z, I);
     set(gcf, 'color', 'white', [0.667, 0.667, 1]);
